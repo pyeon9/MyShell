b2fed0f0ba1a6b702e2ae2375f5ce001da882129
Pyeon9 <50266907+Pyeon9@users.noreply.github.com>
2019-05-25 21:59:54 +0900

    Update shell.c

--- shell.c
+++ shell.c
@@ -9,10 +9,10 @@
 
 #define MAX_LEN_LINE    10
 #define LEN_HOSTNAME	30
-#define COLOR_RED       "\x1b[31m"
-#define COLOR_YELLOW    "\x1b[33m"
-#define COLOR_PURPLE    "\x1b[35m"
-#define COLOR_RESET     "\x1b[0m"
+#define COLOR_RED       "\x1b[31m" // 빨간색
+#define COLOR_YELLOW    "\x1b[33m" // 노란색
+#define COLOR_PURPLE    "\x1b[35m" // 보라색
+#define COLOR_RESET     "\x1b[0m"  // 색 초기화
 
 int main(void)
 {
@@ -39,9 +39,18 @@ int main(void)
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
-	
-	else if (strcmp(s,"exit\n") == 0) {
+
+	else if (!strcmp(s,"exit\n")) { // exit 입력시 shell 종료
 	    break;
+	}
+
+	else if (!strcmp(s,"clear\n")) { // clear 구현
+	    system("clear");
+	    continue;
+	}
+
+	else if (!strcmp(s,"\n")) { // 그냥 enter만 입력 시 아래 수행하지 않음
+	    continue;
 	}
 
         len = strlen(command);
@@ -54,7 +63,7 @@ int main(void)
         printf("[%s]\n", command);
 
 	k = 0;
-	tok[k] = strtok(args[0], "; ");
+	tok[k] = strtok(args[0], "; "); // 공백(space bar입력)과 ; 을 구분자로 하여 입력 command 쪼개기
 	while(tok[k]) {
 	    tok[++k] = strtok(NULL, "; ");
 	}	
@@ -77,10 +86,10 @@ int main(void)
                 }
             }
             else {  /* child */	        
-		printf(COLOR_YELLOW "\n>>프로그램 %s 실행<<\n" COLOR_RESET, tok[k]);
+		printf(COLOR_YELLOW "\n>>프로그램 %s 실행<<\n" COLOR_RESET, tok[k]); // 실행할 프로그램 출력
                 ret = execve(tok[k], args, NULL);
                 if (ret < 0) {	 
-                    fprintf(stderr, COLOR_RED " There is no such program! execve failed\n" COLOR_RESET);
+                    fprintf(stderr, COLOR_RED " There is no such program! execve failed\n" COLOR_RESET); // 해당 프로그램이 없는 경우 안내 메세지 출력
                     return 1;
                 }
             }
